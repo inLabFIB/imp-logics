8e1f4c32c0913fd631812008b88de409d08300d4
XavierOriol <xavier.oriol@upc.edu>
2024-03-29 12:48:49 +0100

    Improve QueryAssert error message

--- src/test/java/edu/upc/fib/inlab/imp/kse/logics/logicschema/assertions/QueryAssert.java
+++ src/test/java/edu/upc/fib/inlab/imp/kse/logics/logicschema/assertions/QueryAssert.java
@@ -4,6 +4,7 @@ import edu.upc.fib.inlab.imp.kse.logics.logicschema.domain.Query;
 import edu.upc.fib.inlab.imp.kse.logics.logicschema.mothers.QueryMother;
 import edu.upc.fib.inlab.imp.kse.logics.logicschema.services.comparator.isomorphism.IsomorphismComparator;
 import edu.upc.fib.inlab.imp.kse.logics.logicschema.services.comparator.isomorphism.IsomorphismOptions;
+import edu.upc.fib.inlab.imp.kse.logics.logicschema.services.printer.QueryPrinter;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.Assertions;
 
@@ -56,7 +57,10 @@ public class QueryAssert extends AbstractAssert<QueryAssert, Query> {
     public QueryAssert isIsomorphicTo(Query expected) {
         boolean result = new IsomorphismComparator(new IsomorphismOptions())
                 .areIsomorphic(actual, expected);
-        Assertions.assertThat(result).isTrue();
+        QueryPrinter queryPrinter = new QueryPrinter();
+        Assertions.assertThat(result)
+                .describedAs("Actual query '" + queryPrinter.print(actual) + "' is not isomorphic to\nExpected query '" + queryPrinter.print(expected) + "'")
+                .isTrue();
         return this;
     }
 
